a1fd3617259dd8f437af100397d5076564950d4f
Griffin A <gdaswegan@gmail.com>
2018-10-08 15:32:52 -0700

    Added the CARS dataset for lab2

---
+++ lab2/Cars/makeCars.sql
@@ -0,0 +1,9 @@
+source CARS-cleanup.sql
+source CARS-setup.sql
+
+source CARS-build-continents.sql
+source CARS-build-countries.sql
+source CARS-build-car-makers.sql
+source CARS-build-model-list.sql
+source CARS-build-car-names.sql
+source CARS-build-cars-data.sql
