c833df4872bd485182674e914786a51f1d8171f5
stbradle <sebradley13@gmail.com>
2018-10-19 15:28:16 -0700

    Added test file for CSU dataset

---
+++ Lab02/turnin/CSU/CSU-test.sql
@@ -0,0 +1,24 @@
+-- Names: Griffen Aswegan & Steven Bradley
+-- CalPoly ID: gaswegan & stbradle
+
+SELECT COUNT(*) as CampusesAct FROM campuses;
+SELECT COUNT(23) as CampusesExp FROM campuses;
+
+
+SELECT COUNT(*) as feesAct FROM fees;
+SELECT COUNT(246) as feesExp FROM fees;
+
+SELECT COUNT(*) as degreesAct FROM degrees;
+SELECT COUNT(320) as degreesExp FROM degrees;
+
+SELECT COUNT(*) as discEnrollAct FROM discEnroll;
+SELECT COUNT(395) as discEnrollExp FROM discEnroll;
+
+SELECT COUNT(*) as disciplinesAct FROM disciplines;
+SELECT COUNT(22) as dicsiplinesExp FROM disciplines;
+
+SELECT COUNT(*) as enrollAct FROM enrollments;
+SELECT COUNT(1113) as enrollExp FROM enrollments;
+
+SELECT COUNT(*) as facultyAct FROM faculty;
+SELECT COUNT(69) as facultyExp FROM faculty;
