806b6793d77a4ba0c9f5c9c66282c6e20dd04697
stbradle <sebradley13@gmail.com>
2018-12-03 15:54:48 -0800

    Added function that executes a query

--- Project/src/ReservationSystemUI.java
+++ Project/src/ReservationSystemUI.java
@@ -11,53 +11,6 @@ public class ReservationSystemUI {
             + "- (Q)uit\n");
    }
 
-   private void ownerLoop() {
-      boolean exit = false;
-
-      while (!exit) {
-         displayOwnerMenu();
-
-//         String[] tokens = input.nextLine().toLowerCase().split("\\s");
-//         char option = tokens[0].charAt(0);
-//         char dataOpt = 0;
-//
-//         if (tokens.length == 2)
-//            dataOpt = tokens[1].charAt(0);
-//
-//         switch(option) {
-//            case 'o':   System.out.println("occupancyMenu\n");
-//               break;
-//            case 'd':   System.out.println("revenueData\n");
-//               break;
-//            case 's':   System.out.println("browseRes()\n");
-//               break;
-//            case 'r':   System.out.println("viewRooms\n");
-//               break;
-//            case 'b':   exit = true;
-//               break;
-//         }
-      }
-   }
-
-   private void guestLoop() {
-      boolean exit = false;
-
-//      while (!exit) {
-//         displayGuestMenu();
-//
-//         char option = input.next().toLowerCase().charAt(0);
-//
-//         switch(option) {
-//            case 'r':   System.out.println("roomsAndRates\n");
-//               break;
-//            case 's':   System.out.println("viewStays\n");
-//               break;
-//            case 'b':   exit = true;
-//               break;
-//         }
-//      }
-   }
-
    private void displayGuestMenu() {
       System.out.println("Welcome, Guest.\n\n"
             + "Choose an option:\n"
@@ -90,6 +43,7 @@ public class ReservationSystemUI {
             + "- (C)lear - Deletes all table contents\n"
             + "- (L)oad - Loads all table contents\n"
             + "- (R)emove - Removes tables\n"
+            + "- (S)witch [subsystem] - "
             + "- (B)ack - Goes back to main menu\n");
 
    }
