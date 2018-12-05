509801327ad76ff043ecae7805de81815d29174c
Griffin <gdaswegan@gmail.com>
2018-12-05 15:20:56 -0800

    Added a 'Room' and 'Reservation' Class

---
+++ Project/src/Reservation.java
@@ -0,0 +1,12 @@
+
+public class Reservation {
+   public int Code;
+   public String Room;
+   public String CheckIn;
+   public String CheckOut;
+   public int Rate;
+   public String LastName;
+   public String Firstname;
+   public int Adults;
+   public int Kids;
+}
---
+++ Project/src/Room.java
@@ -0,0 +1,10 @@
+
+public class Room {
+   public String RoomId;
+   public String RoomName;
+   public int Beds;
+   public String BedType;
+   public int MaxOcc;
+   public int BasePrice;
+   public String Decor;
+}
